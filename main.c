487fbf35a11ccb55c07fb58000f13bfa22ea806c
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-03-15 17:44:43 +0900

    til object

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jaehwkim <jaehwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 12:28:11 by jaehwkim          #+#    #+#             */
-/*   Updated: 2022/03/15 17:27:27 by jaehwkim         ###   ########.fr       */
+/*   Updated: 2022/03/15 17:44:03 by jaehwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,11 @@ int	first_check(char *line, t_check *check)
 	while (line[i] != '\n' && line[i] != '\0')
 	{
 		if (line[i] != '1')
-			return (print_error("ERROR: Wrong map structure!"));
+			return (print_error("1 ERROR: Wrong map structure!"));
 		i++;
 	}
 	if (i == 0)
-		return (print_error("ERROR: Wrong map structure!"));
+		return (print_error("2 ERROR: Wrong map structure!"));
 	check->width = i;
 	return (SUCCESS);
 }
@@ -55,7 +55,7 @@ int	mid_check(char *line, t_check *check)
 	while (line[i] != '\n')
 	{
 		if (i == 0 && line[i] != '1')
-			return (print_error("ERROR: Wrong map structure!"));
+			return (print_error("3 ERROR: Wrong map structure!"));
 		if (line[i] == 'C')
 			check->num_c++;
 		else if (line[i] == 'P')
@@ -63,11 +63,11 @@ int	mid_check(char *line, t_check *check)
 		else if (line[i] == 'E')
 			check->num_e++;
 		else if (i != 0 && (line[i] == '0' || line[i] == '1'))
-			return (print_error("ERROR: Wrong map structure!"));
+			return (print_error("4 ERROR: Wrong map structure!"));
 		i++;
 	}
 	if (line [i - 1] != '1' || i != check->width)
-		return (print_error("ERROR: Wrong map structure!"));
+		return (print_error("5 ERROR: Wrong map structure!"));
 	return (SUCCESS);
 }
 
@@ -79,17 +79,21 @@ int	final_check(char *line, t_check *check)
 	while (line[i] != '\0')
 	{
 		if (line[i] != '1')
-			return (print_error("ERROR: Wrong map structure!"));
+			return (print_error("6 ERROR: Wrong map structure!"));
 		i++;
 	}
 	if (i != check->width)
-		return (print_error("ERROR: Wrong map structure!"));
+		return (print_error("7 ERROR: Wrong map structure!"));
 	return (SUCCESS);
 }
 
 int	object_check(t_check *check)
 {
-	if (check->num_c < 1 || check->num_p > 1 || check->num_e > 1)
-		return (print_error("ERROR: Object Is Not Valid!\n"));
+	if (check->num_c < 1)
+		return (print_error("8 ERROR: Collactavle Is Not Valid!\n"));
+	else if (check->num_p != 1)
+		return (print_error("9 ERROR: Player Is Not Valid!\n"));
+	else if (check->num_e != 1)
+		return (print_error("10 ERROR: Exit Is Not Valid!\n"));
 	return (SUCCESS);
 }
