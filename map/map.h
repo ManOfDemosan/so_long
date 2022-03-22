9e6d9497a921430b1809fe7d1677cd927b1b1c26
Jaehwan Kim <jaehwkim@c3r6s3.42seoul.kr>
2022-03-22 21:15:36 +0900

    유효성검사만남음

--- map/map.h
+++ map/map.h
@@ -1,11 +1,19 @@
 #ifndef MAP_H
 # define MAP_H
 
-#include <stdio.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <unistd.h>
 
-#define SUCCESS 0
-#define FAILURE 1
+# define SUCCESS 0
+# define FAILURE 1
+
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_ESC 53
+# define X_EVENT_KEY_PRESS 2
+// # define X_EVENT_KEY_RELEASE 3
 
 typedef struct s_check{
 	int	width;
@@ -15,7 +23,7 @@ typedef struct s_check{
 	int	num_e;
 	int	status;
 }	t_check;
- 
+
 typedef struct s_ptr{
 	void	*mlx_ptr;
 	void	*win_ptr;
@@ -32,21 +40,29 @@ typedef struct s_info{
 }	t_info;
 
 typedef struct s_img{
-	t_info	*img_ptr0;
-	t_info	*img_ptr1;
+	t_info	*img_ptr_0;
+	t_info	*img_ptr_1;
 	t_info	*img_ptr_P;
 	t_info	*img_ptr_C;
 	t_info	*img_ptr_E;
 }	t_img;
 
+typedef struct s_param{
+	int	x;
+	int	y;
+	int	moves;
+}	t_param;
 
+int		print_error(char *str);
+int		first_check(char *line, t_check *check);
+int		mid_check(char *line, t_check *check);
+int		final_check(char *line, t_check *check);
+int		object_check(t_check *check);
+int		count_hight(char *file, t_check *check);
+int		check_them_all(char *file, t_check *check);
+char	**ft_mapping(char *file, t_check *check);
+void	check_init(t_check *check);
+void	image_init(t_img *img, t_ptr *ptr);
+void	ptr_init(t_ptr *ptr, t_check *check);
 
-int	print_error(char *str);
-int	first_check(char *line, t_check *check);
-int	mid_check(char *line, t_check *check);
-int	final_check(char *line, t_check *check);
-int	object_check(t_check *check);
-int	count_hight(char *file, t_check *check);
-int	check_them_all(char *file, t_check *check);
-char **ft_mapping(char *file, t_check *check);
 #endif
--- mlx.c
+++ mlx.c
@@ -6,7 +6,7 @@
 /*   By: jaehwkim <jaehwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 16:11:22 by jaehwkim          #+#    #+#             */
-/*   Updated: 2022/03/21 22:00:13 by jaehwkim         ###   ########.fr       */
+/*   Updated: 2022/03/22 21:15:16 by jaehwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,76 +40,120 @@ void	check_init(t_check *check)
 	check->num_e = 0;
 }
 
-void image_init(t_img *img, t_ptr *ptr)
+void	image_init(t_img *img, t_ptr *ptr)
 {
 	int		width;
 	int		height;
-	
-	img->img_ptr0 = malloc(sizeof(t_info) * 1);
-	img->img_ptr1 = malloc(sizeof(t_info) * 1);
+
+	img->img_ptr_0 = malloc(sizeof(t_info) * 1);
+	img->img_ptr_1 = malloc(sizeof(t_info) * 1);
 	img->img_ptr_P = malloc(sizeof(t_info) * 1);
 	img->img_ptr_C = malloc(sizeof(t_info) * 1);
 	img->img_ptr_E = malloc(sizeof(t_info) * 1);
-	
-	img->img_ptr0->img = mlx_xpm_file_to_image(ptr->mlx_ptr, "./0.64.xpm", &width, &height);
-	img->img_ptr1->img = mlx_xpm_file_to_image(ptr->mlx_ptr, "./1.64.xpm", &width, &height);
-	img->img_ptr_P->img = mlx_xpm_file_to_image(ptr->mlx_ptr, "./P64.xpm", &width, &height);
-	img->img_ptr_C->img = mlx_xpm_file_to_image(ptr->mlx_ptr, "./C64.xpm", &width, &height);
-	img->img_ptr_E->img = mlx_xpm_file_to_image(ptr->mlx_ptr, "./E64.xpm", &width, &height);
-
+	img->img_ptr_0->img = mlx_xpm_file_to_image(ptr->mlx_ptr, \
+	"./0.64.xpm", &width, &height);
+	img->img_ptr_1->img = mlx_xpm_file_to_image(ptr->mlx_ptr, \
+	"./1.64.xpm", &width, &height);
+	img->img_ptr_P->img = mlx_xpm_file_to_image(ptr->mlx_ptr, \
+	"./P64.xpm", &width, &height);
+	img->img_ptr_C->img = mlx_xpm_file_to_image(ptr->mlx_ptr, \
+	"./C64.xpm", &width, &height);
+	img->img_ptr_E->img = mlx_xpm_file_to_image(ptr->mlx_ptr, \
+	 "./E64.xpm", &width, &height);
 }
-void _ptr(t_ptr *ptr)
+
+void	ptr_init(t_ptr *ptr, t_check *check)
 {
 	ptr->mlx_ptr = mlx_init();
-	ptr->win_ptr = mlx_new_window(ptr->mlx_ptr, 896, 320, "choon!");
+	ptr->win_ptr = mlx_new_window(ptr->mlx_ptr, check->width * 64, check->height * 64, "choon!");
 }
 
-int	main()
+void	param_init(t_param *param)
+{
+	param->x = 0;
+	param->y = 0;
+	param->moves = 0;
+}
+
+int	key_press(int key, t_param *param)
+{
+	if (key != KEY_ESC)
+	{
+		if (key == KEY_W)
+			param->x++;
+		else if (key == KEY_S)
+			param->x--;
+		else if (key == KEY_D)
+			param->y++;
+		else if (key == KEY_A)
+			param->y++;
+		if (key == KEY_W || key == KEY_S || key == KEY_A ||key == KEY_D)
+		{
+			param->moves++;
+			printf("Count: %d\n", param->moves);
+		}
+	}
+	else if (key == KEY_ESC)
+	{
+		printf("So Long, Farewell~ ");
+		exit(0);
+	}
+	return (0);
+}
+
+void	draw_map(t_ptr *ptr, t_img *img, t_param *param, char **map)
+{
+	int		i;
+	int		j;
+
+	param->y = 0;
+	i = 0;
+	while (map[i] != NULL)
+	{
+		param->x = 0;
+		j = 0;
+		while (map[i][j] != '\n')
+		{
+			if (map[i][j] == '1')
+				mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, img->img_ptr_1->img, param->x, param->y);
+			else if (map[i][j] == '0')
+				mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, img->img_ptr_0->img, param->x, param->y);
+			else if (map[i][j] == 'C')
+				mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, img->img_ptr_C->img, param->x, param->y);
+			else if (map[i][j] == 'P')
+				mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, img->img_ptr_P->img, param->x, param->y);
+			else if (map[i][j] == 'E')
+				mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, img->img_ptr_E->img, param->x, param->y);
+			j++;
+			param->x += 64;	
+		}
+		i++;
+		param->y += 64;
+	}
+}
+
+int	main(void)
 {
 	char	**map;
+	int		key;
 	t_check	check;
 	t_ptr	ptr;
 	t_img	*img;
-	
-	_ptr(&ptr);
-	check_init(&check);
-	img = malloc(sizeof (t_img) * 1);
-	image_init(img, &ptr);
+	t_param	param;
 	
 	count_hight("mape.ber", &check);
 	check_them_all("mape.ber", &check);
 	map = ft_mapping("mape.ber", &check);
 
-	int     x;
-	int 	y;
-	int		i;
-	int		j;
-
-	y = 0;
-	i = 0;
-	while (map[i] != NULL)
-	{
-		x = 0;
-		j = 0;
-		while (map[i][j] != '\n')
-		{
-			if (map[i][j] == '1')
-				mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, img->img_ptr1->img, x, y);
-			else if (map[i][j] == '0')
-				mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, img->img_ptr0->img, x, y);
-			else if (map[i][j] == 'C')
-				mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, img->img_ptr_C->img, x, y);
-			else if (map[i][j] == 'P')
-				mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, img->img_ptr_P->img, x, y);
-			else if (map[i][j] == 'E')
-				mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, img->img_ptr_E->img, x, y);
-			j++;
-			x += 64;	
-		}
-		i++;
-		y += 64;
-	}
+	ptr_init(&ptr, &check);
+	check_init(&check);
+	img = malloc(sizeof (t_img) * 1);
+	image_init(img, &ptr);
+	param_init(&param);
+	key_press(key, &param);
 	
+	draw_map(&ptr, img, &param, map);
+	mlx_hook(ptr.win_ptr, X_EVENT_KEY_PRESS, 0, &key_press, &param);
 	mlx_loop(ptr.mlx_ptr);
 	return(0);
 }
